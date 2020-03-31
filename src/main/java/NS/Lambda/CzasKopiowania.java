0d29537b30ca2bc0c71fcc5d2dd7d7ca3bb83534
Natalia <nszarek284@gmail.com>
2020-03-31 21:15:41 +0200

    squash! UML - wszystkie klasy

---
+++ src/main/java/NS/Lambda/CzasKopiowania.java
@@ -0,0 +1,20 @@
+package NS.Lambda;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CzasKopiowania {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double fileCopyTime(FileCopy fileCopy, File src, File trgt) throws IOException {
+        long start = System.currentTimeMillis();
+        return 0;
+    }
+}
+interface FileCopy {
+
+
+}
