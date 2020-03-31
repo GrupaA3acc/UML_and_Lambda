0aa3c01241223016d502ea1be49962db2af450b8
kkaniap <xkaniax@gmail.com>
2020-03-31 19:32:22 +0200

    add 2 copy methods to Copy

--- src/main/java/pk/lambda/Copy.java
+++ src/main/java/pk/lambda/Copy.java
@@ -1,13 +1,12 @@
 package pk.lambda;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 
 @FunctionalInterface
 public interface Copy {
-    void copy(File source, File target);
+    void copy(File source, File target) throws IOException;
 
     Copy useIOStream = (source, target) -> {
         try(FileInputStream is = new FileInputStream(source); FileOutputStream os = new FileOutputStream(target)){
@@ -15,8 +14,18 @@ public interface Copy {
             while ((readingByte = is.read()) != -1){
                 os.write(readingByte);
             }
-        }catch (IOException e){
-            e.printStackTrace();
         }
+    };
+
+    Copy useChannel = (source, target) -> {
+        try(FileChannel sourceChannel = new FileInputStream(source).getChannel();
+            FileChannel targetChannel = new FileOutputStream(target).getChannel()){
+            targetChannel.transferFrom(sourceChannel,0,sourceChannel.size());
+        }
+    };
+
+    Copy useFiles = (source, target) -> {
+        if(!Files.exists(target.toPath()))
+          Files.copy(source.toPath(), target.toPath());
     };
 }
--- src/main/java/pk/lambda/CopyTimer.java
+++ src/main/java/pk/lambda/CopyTimer.java
@@ -1,19 +1,30 @@
 package pk.lambda;
 
 import java.io.File;
+import java.io.IOException;
 
 public class CopyTimer {
     public static void main(String[] args) {
         File source = new File("source.txt");
-        File target = new File("target.txt");
+        File targetIOStream = new File("targetIOStream.txt");
+        File targetChanel = new File("targetChanel.txt");
+        File targetFiles = new File("targetFiles.txt");
 
-        long copyTime = fileCopyTime(Copy.useIOStream, source, target);
-        System.out.println(copyTime);
+        long copyTimeIOStream = fileCopyTime(Copy.useIOStream, source, targetIOStream);
+        long copyTimeChannel = fileCopyTime(Copy.useChannel, source, targetChanel);
+        long copyTimeFiles = fileCopyTime(Copy.useFiles, source, targetFiles);
+        System.out.println("IOStream: " + copyTimeIOStream);
+        System.out.println("Channel: " + copyTimeChannel);
+        System.out.println("Files: " + copyTimeFiles);
     }
 
     public static long fileCopyTime(Copy copyType, File source, File target){
         long start = System.currentTimeMillis();
-        copyType.copy(source, target);
+        try {
+            copyType.copy(source, target);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
         return System.currentTimeMillis() - start;
     }
 }
