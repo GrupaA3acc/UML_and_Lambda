6c3bb27712168a30898c4ffd18c672a73d088be9
Natalia <natikrak@gmail.com>
2020-03-31 19:33:52 +0200

    structure

---
+++ src/main/java/nk/lambda/ZadaniaLambda.java
@@ -0,0 +1,24 @@
+package nk.lambda;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class ZadaniaLambda {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+@Data
+@AllArgsConstructor
+class Person3{
+
+    String name;
+    String lastName;
+    int age;
+    String city;
+
+
+}
